f6e84ea30e6508e2e114e74b8b554292e021dba6
QuartetoeMascotes <79053036+QuartetoeMascotes@users.noreply.github.com>
2021-10-08 20:24:46 -0300

    movimentações e nome

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
 import * as C from "./app.styles";
 import { Character } from "./components/Character";
-
+import { useCharacter } from "./hooks/useCharacter"
 
 const App = () => {
-  return(
+  const char = useCharacter('Miguelito');
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (evt: KeyboardEvent) => {
+    switch (evt.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        char.moveLeft();
+        break;
+        case 'KeyW':
+      case 'ArrowUp':
+        char.moveUp();
+        break;
+        case 'KeyD':
+      case 'ArrowRight':
+        char.moveRight();
+        break;
+        case 'KeyS':
+      case 'ArrowDown':
+        char.moveDown();
+        break;
+    }
+  }
+
+  return (
     <C.Container>
       <C.Map>
-        <Character x={5} y={0}/>
+        <Character x={char.x} y={char.y} side={char.side} name={char.name}/>
       </C.Map>
     </C.Container>
   );
